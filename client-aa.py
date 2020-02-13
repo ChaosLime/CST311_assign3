f613c1b5547042b88cc24e2503b9d31d6894a021
Austin Ah Loo <46932591+aahloo@users.noreply.github.com>
2020-02-13 01:15:56 -0800

    Send and Receive with messages works in Python2.. almost working in Python3

---
+++ client-aa.py
@@ -0,0 +1,68 @@
+
+import socket
+
+import sys
+
+import threading
+
+##client
+
+port = int(sys.argv[1])
+#port = 12000
+
+# Client passed in as commandline argument (X/Y)
+
+#client = int(sys.argv[2])
+client = sys.argv[2]
+
+# Name of client (Alice/Bob)
+
+name = sys.argv[3]
+
+# connect with server at port in commandline argument
+
+def getConnection(port, ip):
+    s = socket.socket()
+    while True:
+        try:
+            s.connect((ip, port))
+            break
+        except:
+            pass
+    return s
+
+# method to send message to server
+
+def sendMessages(s):
+
+    global msgsent
+
+    msgsent = "%s: %s"%(client,name)
+
+    s.send(msgsent.encode()) #Python3 (WORKING)
+    #s.send(msgsent) # Python2 (WORKING)
+
+# receive message from server
+
+def receiveMessages(s):
+
+    msg = s.recv(1024).decode() # Python3 (WORKING)
+    #msg = s.recv(1024)  # Python2 (WORKING)
+
+    print(msgsent)
+
+    print(msg)
+
+def main():
+
+    ip = '127.0.0.1'
+
+    s = getConnection(port,ip)
+
+    t = threading.Thread(target=receiveMessages, args=[s])
+
+    t.start()
+
+    sendMessages(s)
+
+main()
